9a30b140cb9ef0d97ec6f8602342639a1a8017ba
teeeeeeemo <binooooo@naver.com>
2017-02-28 17:23:27 +0900

    꽃 모양 그리기 추가.

--- DrawGraphics/ViewController.swift
+++ DrawGraphics/ViewController.swift
@@ -158,7 +158,36 @@ class ViewController: UIViewController {
         
         imgView.image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
+    }
+    @IBAction func btnDrawFlower(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
         
+        // Draw Triangle
+        context.setLineWidth(3.0)
+        context.setStrokeColor(UIColor.green.cgColor)
+        context.setFillColor(UIColor.green.cgColor)
+        
+        context.move(to: CGPoint(x: 140, y: 200))
+        context.addLine(to: CGPoint(x: 170, y: 450))
+        context.addLine(to: CGPoint(x: 110, y: 450))
+        context.addLine(to: CGPoint(x: 140, y: 200))
+        context.fillPath()
+        context.strokePath()
+        
+        // Draw Circle
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.addEllipse(in: CGRect(x: 90, y: 150, width: 100, height: 100))
+        context.addEllipse(in: CGRect(x: 90+50, y: 150, width: 100, height: 100))
+        context.addEllipse(in: CGRect(x: 90-50, y: 150, width: 100, height: 100))
+        context.addEllipse(in: CGRect(x: 90, y: 150-50, width: 100, height: 100))
+        context.addEllipse(in: CGRect(x: 90, y: 150+50, width: 100, height: 100))
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
         
     }
 
